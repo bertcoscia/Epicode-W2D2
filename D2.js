f8d5af421f4226ab46d97467a9e4de5172bafa42
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-07 16:49:30 +0200

    answered Q14

--- D2.js
+++ D2.js
@@ -221,6 +221,11 @@ console.log(me);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const numbers = [];
+console.log(numbers);
+numbers.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(numbers);
+
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
